026613dc65a4b895837cf027eb31fdc31e53d485
Filip Ginder <42473595+FilipGinder@users.noreply.github.com>
2019-11-27 00:10:34 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script> <!--  DODATAK ZA VIBRACIJU INPUTA PRILIKOM PRAZNOG POLJA-->
+    <link rel="stylesheet" href="dodaci/chosen.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8">
+    <!-- <link rel="stylesheet" href="src/css/style.css"> -->
+    <link rel="stylesheet" href="src/css/style_mob.css">
+    <script type="text/javascript" src="src/js/index_script.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>    <!-- alert-->
+    <title>Auto Gaca Pijaca</title>
+  </head>
+  <body>
+
+    <?php
+       include('pomocni_elementi_stranice/logovanje_forma.php');
+       include('pomocni_elementi_stranice/registracija_forma.php');
+     ?>
+
+
+  </body>
+  </html>
